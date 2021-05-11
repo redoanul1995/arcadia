222cbeaf725dc86715a74a493198aff406718a5a
redoanul1995 <redoanulroni@gmail.com>
2021-05-12 02:12:30 +0600

    database settings

--- arcadia/settings.py
+++ arcadia/settings.py
@@ -77,12 +77,25 @@ WSGI_APPLICATION = 'arcadia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'arcadia',
+            'USER': 'redoanul',
+            'PASSWORD': '5*Kn1EsnoF3f',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+
 
 
 # Password validation
